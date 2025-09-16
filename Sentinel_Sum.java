1612aec9ee2e232c9dcb6f6c3c22c2754bd37684
MarcusCeradini <mnceradini@my.waketech.edu>
2025-09-16 10:17:19 -0400

    Update Sentinel_Sum.java

--- Sentinel_Sum.java
+++ Sentinel_Sum.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Sentinel_Sum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        // Prompt the user to enter a decimal number
-        // Store the input in a variable
         System.out.print("Enter a number: ");
         int user_input = input.nextInt();
         int total_amount = 0 + user_input;
