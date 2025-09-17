ac4fd61e2f234e34abb58d9da6dc2dd3814a85a0
MarcusCeradini <mnceradini@my.waketech.edu>
2025-09-17 09:04:19 -0400

    Add files via upload

---
+++ Number_Stairs.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+
+public class Number_Stairs {
+    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter a number: ");
+//        int user_input = input.nextInt();
+        for (int i=0; i <=10 ; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println("\n");
+        };
+
+    }
+}
