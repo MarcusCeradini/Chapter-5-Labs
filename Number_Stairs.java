ddb0f0402292110dbd7634f3369f08cd82f42083
MarcusCeradini <mnceradini@my.waketech.edu>
2025-09-17 09:48:27 -0400

    Update Number_Stairs.java

--- Number_Stairs.java
+++ Number_Stairs.java
@@ -5,10 +5,14 @@ public class Number_Stairs {
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter a number: ");
 //        int user_input = input.nextInt();
+        //runs unlit i equals 10
         for (int i=0; i <=10 ; i++){
+            // runs until i is greater than j
             for (int j = 1; j <= i; j++){
+                // prints out j for each time the loop is run
                 System.out.print(j);
             }
+            // adds spaceing so the numbers don't get put on the same line
             System.out.println("\n");
         };
 
